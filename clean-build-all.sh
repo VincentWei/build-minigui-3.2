6b07439dfb09bd725a14101c734cd2fe81419f37
VincentWei <vincent@minigui.org>
2018-01-28 09:35:26 +0800

    tune building steps

--- clean-build-all.sh
+++ clean-build-all.sh
@@ -4,12 +4,12 @@ runmode=ths
 
 cd minigui
 ./autogen.sh; ./configure --enable-develmode --with-runmode=$runmode
-make; sudo make install
+sudo make uninstall; make clean; make; sudo make install
 cd ..
 
 for comp in mg-samples mgutils mgplus mgeff mgncs mgncs4touch; do
     cd $comp
     ./autogen.sh; ./configure --enable-develmode
-    make; sudo make install
+    sudo make uninstall; make clean; make; sudo make install
     cd ..
 done

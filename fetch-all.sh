ba37d276f25c4e54e5a8d988a6f70d08f93c01e0
Vincent Wei <vincent@minigui.org>
2019-01-13 14:54:33 +0800

    fetch gvfb from master always

--- fetch-all.sh
+++ fetch-all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-for comp in gvfb minigui-res minigui mg-samples mgutils mgplus mgeff mgncs mgncs4touch cell-phone-ux-demo; do
+# GVFB is always in master branch
+git clone git@github.com:VincentWei/gvfb.git
+
+for comp in minigui-res minigui mg-samples mgutils mgplus mgeff mgncs mgncs4touch cell-phone-ux-demo; do
     git clone git@github.com:VincentWei/$comp.git -b rel-3-2
 done

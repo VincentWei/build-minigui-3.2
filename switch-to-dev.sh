761204dad5baecf55553fad557e40511ac705e04
Vincent Wei <vincent@minigui.org>
2019-01-15 17:16:04 +0800

    tune

--- switch-to-dev.sh
+++ switch-to-dev.sh
@@ -6,5 +6,6 @@ for comp in minigui-res mg-samples minigui mgutils mgplus mgeff mgncs mgncs4touc
     echo Switching $comp to branch $git_branch
     cd $comp
     git checkout $git_branch
+    git pull
     cd ..
 done
